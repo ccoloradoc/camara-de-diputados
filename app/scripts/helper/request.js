2ff7f38ea8e2d9a07aed15b08c78e051bb1a12e8
Cristian Colorado <ccoloradoc@gmail.com>
2017-12-01 14:21:50 -0600

    Change: Request wrapper for network issues

---
+++ app/scripts/helper/request.js
@@ -0,0 +1,25 @@
+const request = require('retry-request');
+
+module.exports = (options, callback) => {
+  request(options, function(err, response, html) {
+    if(!err) {
+      callback(err, response, html)
+    } else {
+      console.log(`Request: First Attempt (Reason: ${err.code})`)
+      // Second attempt
+      request(options, function(err, response, html) {
+        if(!err) {
+          callback(err, response, html)
+        } else {
+          console.log(`Request: Second Attempt (Reason: ${err.code})`)
+          // Second attempt
+          request(options, function(err, response, html) {
+            if(err)
+              console.log(`Request: Third Attempt (Reason: ${err.code})`)
+            callback(err, response, html)
+          });
+        }
+      });
+    }
+  });
+}
