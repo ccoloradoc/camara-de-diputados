80c77833ff65ad1c88ae5e98f71be12d6fa6fb81
Cristian Colorado <ccoloradoc@gmail.com>
2018-01-29 18:12:16 -0600

    Enhancement: Adding zip process

--- app/extract.sh
+++ app/extract.sh
@@ -4,3 +4,4 @@ echo "//////////////////////////////////////////////////////////////////////"
 
 eval mkdir -p data/dump
 eval mysqldump --host mysql -P 3306 -u camara -pcamara camara > data/dump/camara.sql
+eval tar -cvzf data/dump.tar.gz data/dump
