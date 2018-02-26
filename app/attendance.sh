4a1811f8ca5aeba263942bd6ecbdeb4633fbbf6d
Cristian Colorado <ccoloradoc@gmail.com>
2018-02-26 16:45:58 -0600

    Fix: Adding missing attendances

--- app/attendance.sh
+++ app/attendance.sh
@@ -11,8 +11,10 @@ done
 eval "node scripts/scrape-attendance.js from=1001 to=1001"
 eval "node scripts/scrape-attendance.js from=1003 to=1003"
 
+eval "mkdir -p data/sql"
 echo ">> Calculating attendance summary"
-eval "node bin/deputy attendance-summary.js"
+eval "node scripts/deputy-attendance-summary.js"
 
 echo ">> Merge data"
 eval "mysql --host mysql -pcamara -u camara -D camara < data/sql/deputy-attendance-summary.sql"
+eval "mysql --host mysql -pcamara -u camara -D camara < scripts/sql/insert-missing-attendance.sql"
